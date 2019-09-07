527bdb52fad1f01753a7ff6881cae1a8f8e8ac12
Daria <DARIAPIERZGALSKA@gmail.com>
2019-09-07 12:58:48 +0200

    init

---
+++ src/main/java/pl/sda/giftwisher/giftwisher/GiftwisherApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.giftwisher.giftwisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftwisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftwisherApplication.class, args);
+    }
+
+}
