1ff28a826a7c04d9fe84944d272c426cb5d14d12
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-09-14 15:00:01 +0200

    refactor

--- src/main/java/pl/sda/giftwisher/giftwisher/config/DatabaseConfig.java
+++ src/main/java/pl/sda/giftwisher/giftwisher/config/DatabaseConfig.java
@@ -1,10 +1,11 @@
 package pl.sda.giftwisher.giftwisher.config;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 
 @Configuration
