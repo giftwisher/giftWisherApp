f065816eb32ccc7673852b17f5424e0707f7cab7
Daria <DARIAPIERZGALSKA@gmail.com>
2019-09-15 13:30:16 +0200

    - added welcome.html

--- src/main/java/pl/sda/giftwisher/giftwisher/gifts/controller/GiftController.java
+++ src/main/java/pl/sda/giftwisher/giftwisher/gifts/controller/GiftController.java
@@ -2,10 +2,15 @@ package pl.sda.giftwisher.giftwisher.gifts.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.sda.giftwisher.giftwisher.gifts.exceptions.WebApplicationException;
+import pl.sda.giftwisher.giftwisher.gifts.model.GiftStatus;
+import pl.sda.giftwisher.giftwisher.gifts.model.Occassion;
 import pl.sda.giftwisher.giftwisher.gifts.model.dto.GiftDto;
+import pl.sda.giftwisher.giftwisher.gifts.model.dto.NewGiftDto;
 import pl.sda.giftwisher.giftwisher.gifts.service.GiftService;
 
 @Controller
@@ -29,6 +34,25 @@ public class GiftController {
         ModelAndView modelAndView = new ModelAndView("show_one");
         GiftDto giftById = giftService.getGiftById(idGift);
         modelAndView.addObject("gift", giftById);
+        return modelAndView;
+    }
+
+    @GetMapping("/gift_form")
+    public ModelAndView addNewGift() {
+        ModelAndView modelAndView = new ModelAndView("gift_form");
+        NewGiftDto newGiftDto = new NewGiftDto();
+        modelAndView.addObject("gift", newGiftDto);
+        modelAndView.addObject("Occassion", Occassion.values());
+        modelAndView.addObject("GiftStatus", GiftStatus.values());
+        return modelAndView;
+    }
+
+    @PostMapping("/gift_form")
+    public ModelAndView saveGift(@ModelAttribute("gift") NewGiftDto giftToSave, ModelAndView modelAndView) {
+        modelAndView.setViewName("gift_form");
+        giftService.addGift(giftToSave);
+        NewGiftDto newGiftDto = new NewGiftDto();
+        modelAndView.addObject("gift", newGiftDto);
         return modelAndView;
     }
 }
